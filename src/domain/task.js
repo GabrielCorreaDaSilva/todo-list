168a947b938fd7d9a626de9b57c61dad240d8ae5
GabrielCSilva <211619696+GabrielCorreaDaSilva@users.noreply.github.com>
2026-06-23 04:57:07 -0300

    Add notes and checks to update to task

--- src/domain/task.js
+++ src/domain/task.js
@@ -1,4 +1,4 @@
-export function createTask({ type = "task", isComplete = false, parentId = "personal", name, description, dueDate, isImportant, id = crypto.randomUUID() }) {
+export function createTask({ type = "task", isComplete = false, parentId = "personal", notes = "", name, description, dueDate, isImportant, id = crypto.randomUUID() }) {
 
    return {
       getParentId: () => parentId,
@@ -9,15 +9,18 @@ export function createTask({ type = "task", isComplete = false, parentId = "pers
       getName: () => name,
       getDescription: () => description,
       getType: () => type,
-      getStatus:() => isComplete,
+      getStatus: () => isComplete,
       toggleComplete: () => isComplete = !isComplete,
       getIsImportant: () => isImportant,
       getDueDate: () => dueDate,
+      updateNotes: (newNotes) => notes = newNotes,
       update: (data) => {
-         name = data.name;
-         description = data.description;
-         dueDate = data.dueDate;
-         isImportant = data.isImportant;
+         if ("notes" in data) notes = data.notes;
+         if ("name" in data) name = data.name;
+         if ("description" in data) description = data.description;
+         if ("dueDate" in data) dueDate = data.dueDate;
+         if ("isImportant" in data) isImportant = data.isImportant;
+         if ("parentId" in data) parentId = data.parentId;
       }
    };
 }
